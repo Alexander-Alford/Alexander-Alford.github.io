5c0cc04e02a8c73745d2a80b57eb0e3eea25b571
Alexander-Alford <AlexAlford14@yahoo.com>
2021-08-27 21:44:18 -0500

    Atom sim built in. Website update fully completed.

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function openAndClose(open, close){
   document.getElementById(open).style.position = "static";
   document.getElementById(open).style.opacity = 1;  
 }
+
+function embedHtml(html, width, height, target){
+    const embed = document.createElement("iframe");
+
+    embed.setAttribute("src", html);
+    embed.setAttribute("width", width);
+    embed.setAttribute("height", height);
+
+    document.getElementById(target).appendChild(embed);
+}
+
+function clearElem(id){
+  document.getElementById(id).innerHTML = "";
+}
+
+function atomClose(){
+  if(document.getElementById('atom_embed') != null)
+    {
+    clearElem('atom_embed');
+    }
+  document.getElementById('atom_but').innerHTML = "Try It";
+  document.getElementById('atom_but').setAttribute("onclick", "atomOpen();");
+}
+
+function atomOpen(){
+  embedHtml('atom.html', 600, 600, 'atom_embed');
+  document.getElementById('atom_but').innerHTML = "Close";
+  document.getElementById('atom_but').setAttribute("onclick", "atomClose();");
+}
