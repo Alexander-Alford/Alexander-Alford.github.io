bc9af59a1b1e1409ac45b6f603016f93dfbf48d7
Alexander-Alford <alexalford14@yahoo.com>
2022-07-01 23:48:59 -0700

    Moving onclicks to event listeners, styling improvements, moving towards trapping focus.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function openAndClose(open, close){
+var homeButtons = ["but_about", "but_proj", "but_exper", "but_res", "but_git", "but_linkin"];
+var aboutButtons = ["goback_about"];
+var projButtons = ["goback_proj", "but_gameproj", "but_atomproj", "but_tortproj", "but_ecomproj"];
+var gameprojButtons = ["goback_gameproj"]
+var currentSection = "greet_box"
+
+function openSec(open){
   oElem = document.getElementById(open);
-  cElem = document.getElementById(close);
+  cElem = document.getElementById(currentSection);
 
   cElem.style.opacity = 0;
   cElem.style.position = "absolute";
   cElem.style.transition = "opacity 1s";
   oElem.style.position = "static";
-  oElem.style.opacity = 1;  
+  oElem.style.opacity = 1;
+
+  currentSection = open;  
+  disableFocusForAll();
 }
 
 function embedHtml(html, width, height, target){
@@ -39,14 +48,42 @@ function atomOpen(){
   //document.getElementById('atom_but').setAttribute("onclick", "atomClose();");
 }
 
+function enableNormalFocusOnElements(id_list){
+  id_list.forEach(function(elem){ document.getElementById(elem).tabIndex = 0; });
+}
+
+function disableFocusForAll(){
+  let all = homeButtons.concat(projButtons).concat(aboutButtons).concat(gameprojButtons);
+  all.forEach(function(elem){ document.getElementById(elem).tabIndex = -1; });
+}
+
+
+
+function openHome(){
+  openSec('greet_box');
+  enableNormalFocusOnElements(homeButtons);
+}
+
+function openAboutMe(){
+  openSec('aboutme');
+  enableNormalFocusOnElements(aboutButtons);
+}
+
+function openProjects(){
+  openSec('projects');
+  enableNormalFocusOnElements(projButtons);
+}
 
 //Event listener setup.
-document.getElementById("but_1").onclick = function () {openAndClose('aboutme', 'greet_box')};
-document.getElementById("but_2").onclick = function () {window.open('Resume.pdf', '_blank')};
-document.getElementById("but_4").onclick = function () {window.open('https://github.com/Alexander-Alford', '_blank')};
-document.getElementById("but_5").onclick = function () {window.open('https://www.linkedin.com/in/alexanderralford/', '_blank')};
-document.getElementById("but_3").onclick = function () {openAndClose('projects', 'greet_box')};
-document.getElementById("but_6").onclick = function () {openAndClose('projects', 'greet_box')};
+  //Home buttons.
+document.getElementById(homeButtons[0]).onclick = function () { openAboutMe(); };
+document.getElementById(homeButtons[1]).onclick = function () { openProjects(); };
+document.getElementById(homeButtons[2]).onclick = function () {openAndClose('experience', 'greet_box')};
+  //About Me buttons.
+document.getElementById(aboutButtons[0]).onclick = function () { openHome(); };
+  //Project buttons.
+document.getElementById(projButtons[0]).onclick = function () { openHome(); };
+document.getElementById(projButtons[1]).onclick = function () {  };
 
 
-openAndClose("greet_box", "aboutme");
+openHome();
