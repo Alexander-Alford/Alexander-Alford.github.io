a8aba293556b9bc5e14690518b8d38edfd03b305
Alexander-Alford <alexalford14@yahoo.com>
2022-07-07 13:46:16 -0700

    Tabbing trapping mostly working. Needs revision later.

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
 var homeButtons = ["but_about", "but_proj", "but_exper", "but_res", "but_git", "but_linkin"];
 var aboutButtons = ["goback_about"];
-var projButtons = ["goback_proj", "but_gameproj", "but_atomproj", "but_tortproj", "but_ecomproj"];
-var gameprojButtons = ["goback_gameproj"]
+var projButtons = ["but_ecomproj", "but_tortproj", "but_atomproj", "but_gameproj", "goback_proj"];
+var gameprojButtons = ["goback_gameproj"];
+var ecomprojButtons = ["goback_ecomproj", "ecom-proj-link"];
+var turtprojButtons = ["tort-proj-link", "goback_tortproj"];
+var atomprojButtons = ["goback_atomproj"];
+
 var currentSection = "greet_box"
 
-function openSec(open){
+//var firstFocusableElement = homeButtons[0];
+//var lastFocusableElement = homeButtons[5];
+var currentButtonList = homeButtons;
+
+document.addEventListener('keydown', function(e) {
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  console.log(currentButtonList[currentButtonList.length - 1]);
+
+  //Fix for if there is only one button in the list.
+  if(currentButtonList.length === 1){
+    console.log("test");
+    if(document.activeElement.id === currentButtonList[0])
+    {
+      console.log("blur 2");
+      document.getElementById(currentButtonList[0]).blur();
+      e.preventDefault();
+    }
+    else{
+      document.getElementById(currentButtonList[0]).focus();
+      console
+      console.log("focus");
+      e.preventDefault();
+    }
+    return;
+  }
+
+  //If shift + tab is pressed.
+  if (e.shiftKey) { 
+    if (document.activeElement.id === currentButtonList[0]) {
+      document.getElementById(currentButtonList[0]).blur();
+      e.preventDefault();
+    }
+  } 
+  //If tab key alone is pressed.
+  else {
+      console.log(document.activeElement.id, currentButtonList, currentButtonList.length) ;
+    if (document.activeElement.id === currentButtonList[currentButtonList.length - 1]) { 
+      console.log("blur")
+      document.getElementById(currentButtonList[currentButtonList.length - 1]).blur();
+      e.preventDefault();
+    }
+  }
+});
+
+
+
+function openSec(open, butList){
   oElem = document.getElementById(open);
   cElem = document.getElementById(currentSection);
 
@@ -16,16 +71,17 @@ function openSec(open){
 
   currentSection = open;  
   disableFocusForAll();
+  enableNormalFocusOnElements(butList);
 }
 
 function embedHtml(html, width, height, target){
-    const embed = document.createElement("iframe");
+  const embed = document.createElement("iframe");
 
-    embed.setAttribute("src", html);
-    embed.setAttribute("width", width);
-    embed.setAttribute("height", height);
+  embed.setAttribute("src", html);
+  embed.setAttribute("width", width);
+  embed.setAttribute("height", height);
 
-    document.getElementById(target).appendChild(embed);
+  document.getElementById(target).appendChild(embed);
 }
 
 function clearElem(id){
@@ -37,53 +93,52 @@ function atomClose(){
     {
     clearElem('atom_embed');
     }
-  
- // document.getElementById('atom_but').innerHTML = "Try It";
- // document.getElementById('atom_but').setAttribute("onclick", "atomOpen();");
 }
 
 function atomOpen(){
   embedHtml('atom.html', 600, 600, 'atom_embed');
-  //document.getElementById('atom_but').innerHTML = "Close";
-  //document.getElementById('atom_but').setAttribute("onclick", "atomClose();");
 }
 
 function enableNormalFocusOnElements(id_list){
   id_list.forEach(function(elem){ document.getElementById(elem).tabIndex = 0; });
+  //firstFocusableElement = id_list[0];
+  //lastFocusableElement = id_list[id_list.length - 1];
+  currentButtonList = id_list;
+  //console.log(firstFocusableElement, lastFocusableElement);
 }
 
 function disableFocusForAll(){
-  let all = homeButtons.concat(projButtons).concat(aboutButtons).concat(gameprojButtons);
+  let all = homeButtons.concat(projButtons)
+                        .concat(aboutButtons)
+                        .concat(gameprojButtons)
+                        .concat(ecomprojButtons)
+                        .concat(turtprojButtons)
+                        .concat(atomprojButtons);
   all.forEach(function(elem){ document.getElementById(elem).tabIndex = -1; });
 }
 
 
-
-function openHome(){
-  openSec('greet_box');
-  enableNormalFocusOnElements(homeButtons);
-}
-
-function openAboutMe(){
-  openSec('aboutme');
-  enableNormalFocusOnElements(aboutButtons);
-}
-
-function openProjects(){
-  openSec('projects');
-  enableNormalFocusOnElements(projButtons);
-}
-
-//Event listener setup.
-  //Home buttons.
-document.getElementById(homeButtons[0]).onclick = function () { openAboutMe(); };
-document.getElementById(homeButtons[1]).onclick = function () { openProjects(); };
-document.getElementById(homeButtons[2]).onclick = function () {openAndClose('experience', 'greet_box')};
-  //About Me buttons.
-document.getElementById(aboutButtons[0]).onclick = function () { openHome(); };
-  //Project buttons.
-document.getElementById(projButtons[0]).onclick = function () { openHome(); };
-document.getElementById(projButtons[1]).onclick = function () {  };
+//Button setup.
+  //Home
+document.getElementById(homeButtons[0]).onclick = function () { openSec("aboutme", aboutButtons); };
+document.getElementById(homeButtons[1]).onclick = function () { openSec("projects", projButtons); };
+document.getElementById(homeButtons[2]).onclick = function () { openSec("experience", []); };
+  //About Me
+document.getElementById(aboutButtons[0]).onclick = function () { openSec("greet_box", homeButtons); };
+  //Projects
+document.getElementById(projButtons[0]).onclick = function () { openSec("ecomproj", ecomprojButtons); };
+document.getElementById(projButtons[1]).onclick = function () { openSec("tortfolio", turtprojButtons); };
+document.getElementById(projButtons[2]).onclick = function () { openSec("atom_sim", atomprojButtons); atomOpen(); };
+document.getElementById(projButtons[3]).onclick = function () { openSec("game_proj", gameprojButtons); };
+document.getElementById(projButtons[4]).onclick = function () { openSec("greet_box", homeButtons); };
+  //PetShop project 
+document.getElementById(ecomprojButtons[0]).onclick = function () { openSec("projects", projButtons); };
+  //Tortfolio project 
+document.getElementById(turtprojButtons[1]).onclick = function () { openSec("projects", projButtons); };  
+  //Game project 
+document.getElementById(gameprojButtons[0]).onclick = function () { openSec("projects", projButtons); };
+  //Atom project 
+document.getElementById(atomprojButtons[0]).onclick = function () { openSec("projects", projButtons); atomClose(); };
 
 
-openHome();
+openSec("greet_box", homeButtons);
